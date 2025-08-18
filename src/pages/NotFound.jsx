13115983e0b0108db3b74af73429ddcf116e6aea
MinJeong <mineun0517@yu.ac.kr>
2025-08-18 19:15:09 +0900

    [Style/#26]-404페이지 UI 수정

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,25 +6,16 @@ const NotFound = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1
-        className="text-[160px] font-[700] text-blue-800"
-        style={{ color: "#2E47A4" }}
-      >
-        404
-      </h1>
-      <h2
-        className="text-2xl md:text-3xl font-semibold mb-3"
-        style={{ color: "#424242" }}
-      >
+      <h1 className="text-[48px] font-[700] text-[#2E47A4] mb-4">404</h1>
+      <h2 className="text-[24px] font-[500] mb-0.5 text-[#424242]">
         Page Not Found.
       </h2>
-      <p className="mb-11" style={{ color: "#424242" }}>
+      <p className="text-[20px] font-[500] text-[#424242] mb-15">
         Sorry, we couldn't find the page you were looking for.
       </p>
       <button
         onClick={() => navigate("/")}
-        className="px-12 py-2 text-white font-medium rounded-[40px] shadow-md transition cursor-pointer"
-        style={{ backgroundColor: "#2E47A4" }}
+        className="w-[500px] h-[60px] text-[20px] text-white font-semibold rounded-[10px] bg-[#3D489E] transition cursor-pointer"
       >
         Back to home
       </button>
